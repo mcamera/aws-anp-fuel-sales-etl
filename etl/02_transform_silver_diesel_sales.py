589d4b33e5c22a4a573f2a9bad7e1e05eaf59e6a
mcamera <mcamera911@gmail.com>
2022-08-21 18:13:51 -0300

    feat: update the silver layer spark jobs

--- etl/02_transform_silver_diesel_sales.py
+++ etl/02_transform_silver_diesel_sales.py
@@ -14,7 +14,7 @@ spark = (
 )
 
 # Read bronze data
-logging.info("Read bronze layer...")
+logging.info("Read from the bronze layer...")
 diesel_sales = (
     spark.read
     .format("parquet")
@@ -22,62 +22,29 @@ diesel_sales = (
     .load("s3://anp-bronze/diesel_sales/")
 )
 
-# TODO: TRANSFORM DATA
 # Apply some transformations
-# The primary source (Excel sheet) has performed data in integer type. Because of this, we will cast that columns in integer.
 diesel_sales = (
     diesel_sales
     .withColumn('product', F.col('COMBUSTÍVEL'))   
     .withColumn('unit', F.substring(F.col('product'), -3, 2))  # gets the unit
     .withColumn('product', F.expr("substring(product, 1, length(product)-5)"))  # removes the unit from the product name
-    .withColumn('year', F.col('ANO'))
+    .withColumn('year', F.col('ANO').cast(IntegerType()))
     .withColumn('uf', F.col('ESTADO'))
-    .withColumn('jan', F.ceil(F.col('Jan')).cast(IntegerType()))
-    .withColumn('fev', F.ceil(F.col('Fev')).cast(IntegerType()))
-    .withColumn('mar', F.ceil(F.col('Mar')).cast(IntegerType()))
-    .withColumn('abr', F.ceil(F.col('Abr')).cast(IntegerType()))
-    .withColumn('mai', F.ceil(F.col('Mai')).cast(IntegerType()))
-    .withColumn('jun', F.ceil(F.col('Jun')).cast(IntegerType()))
-    .withColumn('jul', F.ceil(F.col('Jul')).cast(IntegerType()))
-    .withColumn('ago', F.ceil(F.col('Ago')).cast(IntegerType()))
-    .withColumn('set', F.ceil(F.col('Set')).cast(IntegerType()))
-    .withColumn('out', F.ceil(F.col('Out')).cast(IntegerType()))
-    .withColumn('nov', F.ceil(F.col('Nov')).cast(IntegerType()))
-    .withColumn('dez', F.ceil(F.col('Dez')).cast(IntegerType()))
-    .withColumn('TOTAL_tmp', F.ceil(F.col('TOTAL')).cast(IntegerType()))
-    .drop('COMBUSTÍVEL', 'ANO', 'ESTADO', 'REGIÃO', 'TOTAL')
+    .withColumn('1', F.col('Jan').cast(DoubleType()))
+    .withColumn('2', F.col('Fev').cast(DoubleType()))
+    .withColumn('3', F.col('Mar').cast(DoubleType()))
+    .withColumn('4', F.col('Abr').cast(DoubleType()))
+    .withColumn('5', F.col('Mai').cast(DoubleType()))
+    .withColumn('6', F.col('Jun').cast(DoubleType()))
+    .withColumn('7', F.col('Jul').cast(DoubleType()))
+    .withColumn('8', F.col('Ago').cast(DoubleType()))
+    .withColumn('9', F.col('Set').cast(DoubleType()))
+    .withColumn('10', F.col('Out').cast(DoubleType()))
+    .withColumn('11', F.col('Nov').cast(DoubleType()))
+    .withColumn('12', F.col('Dez').cast(DoubleType()))
+    .withColumn('total', F.round(F.col('TOTAL').cast(DoubleType()),3))
+    .drop('COMBUSTÍVEL', 'ANO', 'ESTADO', 'REGIÃO','Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez')
 )
-
-# I've noted that the data is shifted. The total column doesn't have the real total count. Maybe it happened due to the file conversion process.
-# Trying to fix the table. It's not right yet.
-
-diesel_sales = (
-    diesel_sales
-    .withColumn('1', F.col('fev'))
-    .withColumn('2', F.col('mar'))
-    .withColumn('3', F.col('abr'))
-    .withColumn('4', F.col('mai'))
-    .withColumn('5', F.col('jun'))
-    .withColumn('6', F.col('jul'))
-    .withColumn('7', F.col('ago'))
-    .withColumn('8', F.col('set'))
-    .withColumn('9', F.col('out'))
-    .withColumn('10', F.col('nov'))
-    .withColumn('11', F.col('dez'))
-    .withColumn('12', F.col('TOTAL_tmp'))
-    .withColumn('total', F.col('jan'))
-    .drop('jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez', 'TOTAL_tmp')
-)
-
-# TODO PIVOT TABLE
-# TODO: APPLY SCHEMA
-#  root
-#  |-- year_month: date
-#  |-- uf: string
-#  |-- product: string
-#  |-- unit: string
-#  |-- volume: double
-#  |-- created_at: timestamp
 
 # Write table in silver layer with delta format
 logging.info("Writing delta table into the silver layer...")
