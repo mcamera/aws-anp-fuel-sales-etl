3c4a2f58003c3c4ba2c8674a9598e422b151f1f1
mcamera <mcamera911@gmail.com>
2022-08-21 16:48:12 -0300

    feat: fix the shifted data from the source sheet.

--- airflow/include/extract_vendas_combustiveis.py
+++ airflow/include/extract_vendas_combustiveis.py
@@ -48,13 +48,49 @@ class extractVendasCombustiveis:
             logging.error('Error when trying to convert the file.')
             self.remove_temp_files()
             raise
+    
+    def unshift(self, data: list) -> list:
+        """Fix the shifted data problem.
+        Cycles between the values and create a new variable with a shift of 1 column.
+        The shift index increases in 1 for each row and ends the cyclic at the 13th column.
 
-    def upload_sheet_to_s3(self, sheet_name: str, tablename: str) -> None:
-        # Upload the sheet to the S3 bronze bucket
+        Args:
+            data (list): Values for each month and total for the record
+
+        Returns:
+            (list): Data with the column's order fixed.
+        """
+        global idx_shift
+        if idx_shift > 12:
+            idx_shift = 1  # Resets cyclic the shift index if the month range reaches the end
+        else:
+            idx_shift += 1  # Increase the data shift index for the actual row
+
+        data_fixed = [0]*13  # Init the fixed data variable
+        for i in range(13):
+            idx = i + idx_shift
+            if idx > 12:
+                idx = idx - 13  # Resets the cyclic the month range if it reaches the end
+            data_fixed[i] = data[idx]
+
+        return data_fixed
+
+    def fix_and_upload_sheet_to_s3(self, sheet_name: str, tablename: str) -> None:
+        """ unshift the sheet and upload to the S3 bronze bucket
+
+        Args:
+            sheet_name (str): Sheet's name to be extracted.
+            tablename (str): Table's name to be used in the parquet file.
+        """
         filepath = f"{tablename}.parquet"
 
         try:
             df = pd.read_excel(f'./converted/{self.filename}', sheet_name=sheet_name)
+
+            # Unshift the data
+            for rowindex, row in df.iterrows():
+                df.iloc[rowindex, 4:17] = self.unshift(row[4:17].values.flatten().tolist())
+
             df.to_parquet(path=filepath)
             boto3.resource('s3').Bucket('anp-bronze').upload_file(filepath, filepath)
             return
@@ -64,10 +100,15 @@ class extractVendasCombustiveis:
             raise
     
     def get_vendas_combustiveis(self):
+        """Main function for the data extraction
+        """
+        global idx_shift
+        idx_shift = 0
+
         self.download_file()
         self.convert_file()
-        self.upload_sheet_to_s3(sheet_name='DPCache_m3', tablename='sales_oil_fuels')
-        self.upload_sheet_to_s3(sheet_name='DPCache_m3_2', tablename='sales_diesel')
+        self.fix_and_upload_sheet_to_s3(sheet_name='DPCache_m3', tablename='sales_oil_fuels')
+        self.fix_and_upload_sheet_to_s3(sheet_name='DPCache_m3_2', tablename='sales_diesel')
         self.remove_temp_files()
 
 
@@ -78,8 +119,10 @@ if __name__ == '__main__':
 
     aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
     aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-    s3client = boto3.client("s3", aws_access_key_id=aws_access_key_id,
-                          aws_secret_access_key=aws_secret_access_key)
+    s3client = boto3.client(
+        "s3", aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+        )
 
     pipeline = extractVendasCombustiveis(s3client)
     pipeline.get_vendas_combustiveis()
